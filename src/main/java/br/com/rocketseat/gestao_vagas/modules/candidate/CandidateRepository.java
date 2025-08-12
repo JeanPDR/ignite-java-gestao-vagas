0d1cce8f3e4884493cec98ae8051717057e21028
Faculdade Embu das Artes <90737370+JeanPDR@users.noreply.github.com>
2025-08-11 21:33:14 -0300

    :sparkles: feat: implementar use case de cadastro de candidato

--- src/main/java/br/com/rocketseat/gestao_vagas/modules/candidate/CandidateRepository.java
+++ src/main/java/br/com/rocketseat/gestao_vagas/modules/candidate/CandidateRepository.java
@@ -1,11 +1,13 @@
 package br.com.rocketseat.gestao_vagas.modules.candidate;
 
+
 import java.util.UUID;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID>{
 
-
+ Optional<CandidateEntity> findByUsernameOrEmail(String username, String email);
 
 }
