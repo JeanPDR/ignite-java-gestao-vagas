8d4dfafd010661e660281c7fa7b470b1e1033a8f
Faculdade Embu das Artes <90737370+JeanPDR@users.noreply.github.com>
2025-08-12 21:37:11 -0300

    :sparkles: feat: criando entidade e tabela no banco de dados chamada campany

---
+++ src/main/java/br/com/rocketseat/gestao_vagas/modules/company/entities/CompanyEntity.java
@@ -0,0 +1,42 @@
+package br.com.rocketseat.gestao_vagas.modules.company.entities;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+
+@Entity(name = "company")
+@Data
+public class CompanyEntity {
+
+
+     @Id
+     @GeneratedValue(strategy = GenerationType.UUID)
+     private UUID id;
+     @NotBlank()
+     @Pattern(regexp= "\\S+", message= "O Campo [username] não pode contém espaços em branco")
+     private String username;
+
+     @Email(message= "O Campo [email] dever ser um email válido")
+     private String email;
+
+     @Length(min= 6, message= "O Campo(senha) deve ter no mínimo 6 caracteres")
+     private String password;
+     
+     private String website;
+     private String name;
+     private String description;
+
+     private LocalDateTime createdAt;
+     
+}
